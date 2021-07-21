86373607e2cbc08c304850e0451e91770ada51af
Vegard Stikbakke <vegard.stikbakke@gmail.com>
2021-07-21 21:13:33 +0200

    Lex comma

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -8,7 +8,7 @@ import (
 )
 
 func TestExpressionValue(t *testing.T) {
-	input := "1 + 2 * (30 / 5) - 1 + 3.14 + 'abc' 1.0 'def' select SELECT SeLeCT aWord"
+	input := "1 + 2 * (30 / 5) - 1 + 3.14 + 'abc' 1.0 'def' select SELECT SeLeCT aWord ,"
 	tests := []struct {
 		expectedType    token.TokenType
 		expectedLiteral string
@@ -34,6 +34,7 @@ func TestExpressionValue(t *testing.T) {
 		{token.SELECT, "SELECT"},
 		{token.SELECT, "SELECT"},
 		{token.IDENTIFIER, "aWord"},
+		{token.COMMA, ","},
 	}
 	l := lexer.New(input)
 	for i, tt := range tests {
