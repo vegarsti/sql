5032fbdf86c71a3e4e80fe1f51ed999f1fdd804f
Vegard Stikbakke <vegard.stikbakke@gmail.com>
2021-08-13 20:52:46 +0200

    Add SortValue() flaot64 to the Object interface

--- object/object.go
+++ object/object.go
@@ -2,6 +2,7 @@ package object
 
 import (
 	"fmt"
+	"math"
 	"strings"
 )
 
@@ -20,11 +21,13 @@ const (
 type Object interface {
 	Type() ObjectType
 	Inspect() string
+	SortValue() float64
 }
 
 type Row struct {
-	Aliases []string
-	Values  []Object
+	Aliases      []string
+	Values       []Object
+	SortByValues []Object
 }
 
 func (r *Row) Inspect() string {
@@ -34,7 +37,8 @@ func (r *Row) Inspect() string {
 	}
 	return strings.Join(values, "\t")
 }
-func (r *Row) Type() ObjectType { return ROW_OBJ }
+func (r *Row) Type() ObjectType   { return ROW_OBJ }
+func (r *Row) SortValue() float64 { panic("a row doesn't have a sort value") }
 
 type Result struct {
 	Aliases []string
@@ -52,7 +56,8 @@ func (r *Result) Inspect() string {
 		allRowsString,
 	}, "\n")
 }
-func (r *Result) Type() ObjectType { return RESULT_OBJ }
+func (r *Result) Type() ObjectType   { return RESULT_OBJ }
+func (r *Result) SortValue() float64 { panic("a result doesn't have a sort value") }
 
 type DataType string
 
@@ -76,15 +81,17 @@ type Integer struct {
 	Value int64
 }
 
-func (i *Integer) Inspect() string  { return fmt.Sprintf("%d", i.Value) }
-func (i *Integer) Type() ObjectType { return INTEGER_OBJ }
+func (i *Integer) Inspect() string    { return fmt.Sprintf("%d", i.Value) }
+func (i *Integer) Type() ObjectType   { return INTEGER_OBJ }
+func (i *Integer) SortValue() float64 { return float64(i.Value) }
 
 type Float struct {
 	Value float64
 }
 
-func (f *Float) Inspect() string  { return fmt.Sprintf("%f", f.Value) }
-func (f *Float) Type() ObjectType { return FLOAT_OBJ }
+func (f *Float) Inspect() string    { return fmt.Sprintf("%f", f.Value) }
+func (f *Float) Type() ObjectType   { return FLOAT_OBJ }
+func (f *Float) SortValue() float64 { return f.Value }
 
 type String struct {
 	Value string
@@ -93,15 +100,29 @@ type String struct {
 func (s *String) Inspect() string  { return "'" + s.Value + "'" }
 func (s *String) Type() ObjectType { return STRING_OBJ }
 
+// SortValue of a string is u * 10^-i,
+// where u is the unicode value of the rune,
+// and i is the index of the rune in the string
+func (s *String) SortValue() float64 {
+	sortValue := float64(0)
+	for i, r := range s.Value {
+		unicodeValue := int(r)
+		sortValue += float64(unicodeValue) * math.Pow10(-i)
+	}
+	return sortValue
+}
+
 type Error struct {
 	Message string
 }
 
-func (e *Error) Type() ObjectType { return ERROR_OBJ }
-func (e *Error) Inspect() string  { return "ERROR: " + e.Message }
+func (e *Error) Type() ObjectType   { return ERROR_OBJ }
+func (e *Error) Inspect() string    { return "ERROR: " + e.Message }
+func (e *Error) SortValue() float64 { panic("an error doesn't have a sort value") }
 
 type OK struct {
 }
 
-func (ok *OK) Type() ObjectType { return OK_OBJ }
-func (ok *OK) Inspect() string  { return "OK" }
+func (ok *OK) Type() ObjectType   { return OK_OBJ }
+func (ok *OK) Inspect() string    { return "OK" }
+func (ok *OK) SortValue() float64 { panic("an OK doesn't have a sort value") }
