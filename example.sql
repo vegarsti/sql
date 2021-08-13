dfa5d05632de32a407d15453b5eb25781a1c29ce
Vegard Stikbakke <vegard.stikbakke@gmail.com>
2021-08-13 20:54:22 +0200

    Add example.sql to make it easier to get started with a table

---
+++ example.sql
@@ -0,0 +1,5 @@
+create table programming_languages (name text, first_appeared integer);
+insert into programming_languages values ('C', 1972);
+insert into programming_languages values ('Python', 1990);
+insert into programming_languages values ('Lisp', 1958);
+insert into programming_languages values ('Go', 2009);
