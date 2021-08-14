0385d13f6d033e271f314cffc4a125f63cff673b
Vegard Stikbakke <vegard.stikbakke@gmail.com>
2021-08-14 14:26:08 +0200

    Add true and false keywords

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -10,7 +10,7 @@ import (
 func TestExpressionValue(t *testing.T) {
 	input := `
 1 + 2 * (30 / 5) - 1 + 3.14 + 'abc' 1.0 'def' select SELECT SeLeCT aWord , AS as aS As create table text double integer insert into values from identifier_with_underscore;
-order by desc asc
+order by desc asc false true
 `
 	tests := []struct {
 		expectedType    token.TokenType
@@ -57,6 +57,8 @@ order by desc asc
 		{token.BY, "BY"},
 		{token.DESC, "DESC"},
 		{token.ASC, "ASC"},
+		{token.BOOL, "FALSE"},
+		{token.BOOL, "TRUE"},
 	}
 	l := lexer.New(input)
 	for i, tt := range tests {
